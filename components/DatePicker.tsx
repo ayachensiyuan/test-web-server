5d38ea836ebd81f02bb8d35b0e7848cdc3bea79a
= <=>
2022-08-23 13:43:18 +0800

    update timePicker

--- components/DatePicker.tsx
+++ components/DatePicker.tsx
@@ -1,13 +1,40 @@
-export default function DatePicker() {
+export default function DatePicker(opts: { timeList: {month: string, year: number}[] }) {
+    let currentCursor = opts.timeList.length - 1
+    const baseClass = ' mx-5 flex justify-center items-center w-10 h-10 border-1 border-gray-200 rounded shadow'
+    const rightEndClass =' '
+    const rightClass = ' active:pl-2 hover:cursor-pointer hover:bg-gray-100 '
+    const leftEndClass ='  pl-2'
+    const leftClass = ' active:pr-2 hover:cursor-pointer pl-2 hover:bg-gray-100'
+    let rightPointClass = ''
+    let leftPointClass = ''
+    if  (currentCursor === opts.timeList.length - 1) {
+        rightPointClass = rightEndClass + ' ' + baseClass
+        leftPointClass = leftClass + ' ' + baseClass
+    } else if (currentCursor === 0) {
+        rightPointClass = rightClass + ' ' + baseClass
+        leftPointClass = leftEndClass + ' ' + baseClass
+    } else {
+        rightPointClass = rightClass + ' ' + baseClass
+        leftPointClass = leftClass + ' ' + baseClass
+    }
+
     return (
         <div className=" flex w-90  h-10 md-mt-10 md-ml-10 my-7 justify-between items-center scale-90 md-scale-100">
-            <div className="mx-5 flex justify-center items-center w-10 h-10 border-1 border-gray-200 rounded">
-                <img src="https://github.githubassets.com/favicons/favicon.svg" className="w-6 h-6" />
+
+            <div className={leftPointClass} >
+                <span className={currentCursor === 0?"material-icons text-gray":"material-icons"}>
+                    arrow_back_ios
+                </span>
             </div>
-            <p className="text-sm">June 2022 to August 2022</p>
-            <div className="mx-5 flex justify-center items-center w-10 h-10 border-1 border-gray-200 rounded">
-                <img src="https://github.githubassets.com/favicons/favicon.svg" className="w-6 h-6" />
+
+            <p className="md-text-lg text-sm">{opts.timeList[1].month + ' ' + opts.timeList[1].year + ' to ' + opts.timeList[3].month + ' ' + opts.timeList[3].year}</p>
+
+            <div className={rightPointClass}>
+                <span className={currentCursor === opts.timeList.length - 1?"material-icons text-gray":"material-icons"} >
+                    arrow_forward_ios
+                </span>
             </div>
+
         </div>
     );
 }
--- routes/history.tsx
+++ routes/history.tsx
@@ -2,12 +2,12 @@ import DatePicker from "../components/DatePicker.tsx"
 import IncidentEvent from "../components/IncidentEvent.tsx"
 import {Link} from "aleph/react";
 export default function Mypage() {
+  const historyPageTitle = 'Incident History'
 return (
     <div className="w-9/10 max-w-250 mx-auto  ">
-        <h1 className="text-3xl md-pb-8 pb-4">Incident History</h1>
+        <h1 className="text-3xl md-pb-8 pb-4">{historyPageTitle}</h1>
         <div className="border-gray-200 border-1.5  bg-white shadow-md shadow-gray-200 ">
-            <DatePicker />
-
+            <DatePicker timeList={[{month:'Aug', year: 2022},{month:'Jul', year: 2022},{month:'Jun', year: 2022},{month:'May', year: 2022},]}/>
             <div>
                 <IncidentEvent />
                 <IncidentEvent />
