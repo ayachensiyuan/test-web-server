0258c326987da63e8df1f37a5965afc7bd262265
= <=>
2022-08-29 00:03:38 +0800

    working on getreport api

--- routes/_export.ts
+++ routes/_export.ts
@@ -5,22 +5,26 @@ import * as $0 from "./_404.tsx";
 import * as $1 from "./_app.tsx";
 import * as $2 from "./index.tsx";
 import * as $3 from "./history.tsx";
-import * as $4 from "./api/historydata.ts";
-import * as $5 from "./api/testdata.ts";
-import * as $6 from "./api/testCase.ts";
-import * as $7 from "./report/$reportId.tsx";
-import * as $8 from "./api/report/index.ts";
-import * as $9 from "./api/report/$reportId.ts";
+import * as $4 from "./api/todayreportstatus.ts";
+import * as $5 from "./api/historydata.ts";
+import * as $6 from "./api/todayreport.ts";
+import * as $7 from "./api/testdata.ts";
+import * as $8 from "./api/testCase.ts";
+import * as $9 from "./report/$reportId.tsx";
+import * as $10 from "./api/report/index.ts";
+import * as $11 from "./api/report/$reportId.ts";
 
 export default {
   "/_404": $0,
   "/_app": $1,
   "/": $2,
   "/history": $3,
-  "/api/historydata": $4,
-  "/api/testdata": $5,
-  "/api/testCase": $6,
-  "/report/:reportId": $7,
-  "/api/report/index": $8,
-  "/api/report/:reportId": $9,
+  "/api/todayreportstatus": $4,
+  "/api/historydata": $5,
+  "/api/todayreport": $6,
+  "/api/testdata": $7,
+  "/api/testCase": $8,
+  "/report/:reportId": $9,
+  "/api/report/index": $10,
+  "/api/report/:reportId": $11,
 };
